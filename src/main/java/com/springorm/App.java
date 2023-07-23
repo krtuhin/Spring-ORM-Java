3b66712091f655bde492d216f095a6ce805cbf13
krtuhin <tuhin.km2774@gmail.com>
2023-07-23 22:15:21 +0530

    CRUD Operation using Spring ORM

--- src/main/java/com/springorm/App.java
+++ src/main/java/com/springorm/App.java
@@ -2,6 +2,7 @@ package com.springorm;
 
 import com.springorm.dao.StudentDao;
 import com.springorm.entities.Student;
+import java.util.List;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -15,6 +16,26 @@ public class App {
 
         Student student = context.getBean("st", Student.class);
 
+        //call insert method
         int r = studentDao.insert(student);
+
+        //call single object select query
+        Student st = studentDao.getStudent(543);
+
+        System.out.println("Selected: " + st);
+
+        //call multiple object query
+        List<Student> list = studentDao.getStudents();
+
+        System.out.println("Selected : " + list);
+
+        //call delete query
+        studentDao.delete(5234);
+
+        System.out.println("Deleted...!");
+
+        Student stt = new Student(5447, "Sourav", "Kolkata");
+
+        studentDao.updateStudent(stt);
     }
 }
--- src/main/java/com/springorm/dao/StudentDao.java
+++ src/main/java/com/springorm/dao/StudentDao.java
@@ -1,6 +1,7 @@
 package com.springorm.dao;
 
 import com.springorm.entities.Student;
+import java.util.List;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,5 +20,40 @@ public class StudentDao {
         int r = (int) this.hibernateTemplate.save(student);
 
         return r;
+    }
+
+    //select single object
+    public Student getStudent(int id) {
+
+        Student st = this.hibernateTemplate.get(Student.class, id);
+
+        return st;
+    }
+
+    //select multiple object
+    public List<Student> getStudents() {
+
+        List<Student> students = this.hibernateTemplate.loadAll(Student.class);
+
+        return students;
+    }
+
+    //delete method
+    @Transactional
+    public void delete(int id) {
+
+        Student student = this.hibernateTemplate.get(Student.class, id);
+
+        this.hibernateTemplate.delete(student);
+    }
+
+    //update method
+    @Transactional
+    public void updateStudent(Student student) {
+
+        //any of these two method
+        this.hibernateTemplate.saveOrUpdate(student);
+
+        //this.hibernateTemplate.update(student);
     }
 }
